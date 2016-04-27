545e0cc7c858963bec4da9239c67ddaa035fec16
Rudy Rigot <rudy.rigot@gmail.com>
2016-04-27 12:26:55 -0700

    Validating that outstanding balance is enough for new payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,10 @@ class Payment < ActiveRecord::Base
   belongs_to :loan
 
   validates :loan, :date, :amount, presence: true
+
+  validate :cannot_exceed_outstanding_amount
+
+  def cannot_exceed_outstanding_amount
+    errors.add(:amount, "exceeds loan's outstanding amount") if self.loan.outstanding_amount < self.amount
+  end
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:loan) {
+    loan = Loan.create!(funded_amount: 100.0)
+    # Payment can't be created with their own let, would never run because of lazy evaluation
+    Payment.create!(date: 1.day.ago, amount: 15, loan: loan)
+    Payment.create!(date: Date.today, amount: 10, loan: loan)
+    loan
+  }
+
+  it "cannot exceed outstanding amount" do
+    new_payment = Payment.create(date: Date.today, amount: 80, loan: loan)
+    expect(new_payment.errors.size).to eq 1
+    expect(new_payment.errors[:amount][0]).to eq "exceeds loan's outstanding amount"
+  end
 end
