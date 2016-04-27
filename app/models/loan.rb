3ac28c8aa92b4b3828f651a36f7875f02baadda0
Rudy Rigot <rudy.rigot@gmail.com>
2016-04-27 12:17:06 -0700

    Loan#outstanding_amount

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,4 +2,8 @@ class Loan < ActiveRecord::Base
   has_many :payments, dependent: :destroy
 
   validates :funded_amount, presence: true
+
+  def outstanding_amount
+    funded_amount - Payment.where(loan: self).sum(:amount)
+  end
 end
---
+++ spec/models/loan_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Loan, type: :model do
+  describe "outstanding_amount" do
+
+    let(:loan) {
+      loan = Loan.create!(funded_amount: 100.0)
+      # Payment can't be created with their own let, would never run because of lazy evaluation
+      Payment.create!(date: 1.day.ago, amount: 15, loan: loan)
+      Payment.create!(date: Date.today, amount: 10, loan: loan)
+      loan
+    }
+
+    it "returns the proper amount" do
+      expect(loan.outstanding_amount).to eq 75.0
+    end
+  end
+end
