9749758d7c0bdd44d7f75bd2b4d6f0255f9c0700
Rudy Rigot <rudy.rigot@gmail.com>
2016-04-27 22:36:23 -0700

    Ensuring no 500 on non-existing actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :payments, defaults: {format: :json}
-  resources :loans, defaults: {format: :json} do
+  resources :payments, defaults: {format: :json}, only: [:show, :index]
+  resources :loans, defaults: {format: :json}, only: [:show, :index] do
     get 'payments', on: :member
   end
 end
